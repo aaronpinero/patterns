7a995ae8d7976844f0e4174153fc6dd725d1fd6e
Aaron Pinero <aaronpinero@gmail.com>
2020-02-03 13:31:17 -0500

    update lazyimage.js to remove console .log statements

--- source/patterns/lazyimage/lazyimage.js
+++ source/patterns/lazyimage/lazyimage.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   if ("IntersectionObserver" in window) {
-    var lazyImages = [].slice.call(document.querySelectorAll("img[data-lazyimage]")); console.log({lazyImages});
+    var lazyImages = [].slice.call(document.querySelectorAll("img[data-lazyimage]")); // console.log({lazyImages});
 
     lazyImages.forEach(function(lazyImage) {
       lazyImage.classList.add("not-lazyLoaded");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (entry.isIntersecting) {
           let lazyImage = entry.target;
           let loadImage = new Image();
-          let imgSrc = lazyImage.getAttribute('data-lazyimage'); console.log({imgSrc});
+          let imgSrc = lazyImage.getAttribute('data-lazyimage'); // console.log({imgSrc});
           loadImage.onload = function(){
             lazyImage.setAttribute('src',imgSrc);
             lazyImage.classList.remove("not-lazyLoaded");
