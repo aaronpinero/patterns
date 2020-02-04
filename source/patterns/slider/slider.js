a381b5498fd481d940463946736ec340d683b3c7
Aaron Pinero <aaronpinero@gmail.com>
2020-02-03 21:52:46 -0500

    finished basic code for basic slider; issue remain

--- source/patterns/slider/slider.js
+++ source/patterns/slider/slider.js
@@ -5,18 +5,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // function to scroll to a certain slide
     var scrollIt = function scrollIt(slider,slideToShow) {
-      console.log("called Scrollit");
-      var slides = slider.querySelectorAll('li');
+      var slides = slider.querySelectorAll('.slide');
       var sliderPort = slider.querySelector('.slider-port');
       var scrollPos = slideToShow.offsetLeft;
       sliderPort.scrollLeft = scrollPos;
-      setTimeout(function(){ sliderPort.scrollLeft = scrollPos; },50);
+      setTimeout(function(){ sliderPort.scrollLeft = scrollPos; },75);
     };
 
     // function to show a slide
     var showSlide = function showSlide(slider,direction) {
       var sliderPort = slider.querySelector('.slider-port');
-      var slides = slider.querySelectorAll('li');
+      var slides = slider.querySelectorAll('.slide');
       var visible = slider.querySelectorAll('.visible');
       var max_visible = Math.round(sliderPort.offsetWidth / slides[0].offsetWidth);
       var i = direction === 'previous' ? 0 : 1;
@@ -42,8 +41,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // do for each slider
     sliders.forEach(function(slider) {
+      var sliderPort = slider.querySelector('.slider-port');
+
       // find all the slides
-      var slides = slider.querySelectorAll('li');
+      var slides = slider.querySelectorAll('.slide');
 
       // set the default tab index such that the slide links cannot be focused
       Array.prototype.forEach.call(slides, function (slide) {
@@ -52,41 +53,72 @@ document.addEventListener("DOMContentLoaded", function() {
 
       // settings for intersection observer
       var observerSettings = {
-        root: slider,
-        rootMargin: '0px'
+        root: sliderPort
       };
 
-      var observerCallback = function callback(slides, observer) {
+      // observe for when slide is visible
+      var slide_observer = new IntersectionObserver(function callback(slides, observer) {
         Array.prototype.forEach.call(slides, function (entry) {
-          entry.target.classList.remove('visible');
           var link = entry.target.querySelector('a');
-          link.setAttribute('tabindex', '-1');
-          if (!entry.intersectionRatio > 0) {
-            return;
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            link.removeAttribute('tabindex', '-1');
           }
-          entry.target.classList.add('visible');
-          link.removeAttribute('tabindex', '-1');
+          else {
+            entry.target.classList.remove('visible');
+            link.setAttribute('tabindex', '-1');
+          }
         });
-      };
+      }, observerSettings);
 
-      var slide_observer = new IntersectionObserver(observerCallback, observerSettings);
       Array.prototype.forEach.call(slides, function(slide) {
         return slide_observer.observe(slide);
       });
+
+      // observe for the start of the slides
+      var start_observer = new IntersectionObserver(function callback(slides, observer) {
+        Array.prototype.forEach.call(slides, function (entry) {
+          if (entry.isIntersecting) {
+            entry.target.parentNode.parentNode.parentNode.querySelector('.slider-control-prev').setAttribute('disabled','disabled');
+          }
+          else {
+            entry.target.parentNode.parentNode.parentNode.querySelector('.slider-control-prev').removeAttribute('disabled');
+          }
+        });
+      }, observerSettings);
+
+      start_observer.observe(slides[0]);
+
+      // observe for the end of the slides
+      var end_observer = new IntersectionObserver(function callback(slides, observer) {
+        Array.prototype.forEach.call(slides, function (entry) {
+          if (entry.isIntersecting) {
+            entry.target.parentNode.parentNode.parentNode.querySelector('.slider-control-next').setAttribute('disabled','disabled');
+          }
+          else {
+            entry.target.parentNode.parentNode.parentNode.querySelector('.slider-control-next').removeAttribute('disabled');
+          }
+        });
+      }, observerSettings);
+
+      end_observer.observe(slides[slides.length - 1]);
 
       // add controls
       var controls = document.createElement('ul');
       controls.setAttribute('aria-label', 'slider controls');
       controls.className = "slider-controls list-unstyled p-0 m-0";
-      controls.innerHTML = '\n  <li class="d-block p-0 m-0"><button class="slider-control slider-control-prev" aria-label="previous">Previous</button></li>\n  <li class="d-block p-0 m-0"><button class="slider-control slider-control-next" aria-label="next">Next</button></li>';
+      controls.innerHTML = '\n  <li class="d-block p-0 m-0"><button class="slider-control slider-control-prev arrow prev" aria-label="previous">Previous</button></li>\n  <li class="d-block p-0 m-0"><button class="slider-control slider-control-next arrow next" aria-label="next">Next</button></li>';
       slider.append(controls);
-      controls.addEventListener('click', function (e) {
+      var prev = controls.querySelector('.slider-control-prev');
+      var next = controls.querySelector('.slider-control-next');
+      prev.addEventListener('click', function(e) {
         e.preventDefault();
-        showSlide(slider,e.target.closest('button').getAttribute('aria-label'));
+        showSlide(slider,'previous');
       });
-      
-  
+      next.addEventListener('click', function(e) {
+        e.preventDefault();
+        showSlide(slider,'next');
+      });
     });
-
   }
 });
